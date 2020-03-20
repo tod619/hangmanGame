8e79f5860c97e7e7f80cd3175c40a2f1aee4fbe8
tod619 <trevorodwyer619@hotmail.com>
2020-03-20 14:45:27 +0000

    Add variables to app.js file

---
+++ app.js
@@ -0,0 +1,7 @@
+// Add Variables
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-again");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
